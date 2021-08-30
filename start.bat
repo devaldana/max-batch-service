4d60123d736833c46723cb9a21fe1484984fe631
David Aldana <davidaldana@cloud.upwork.com>
2021-08-30 09:04:30 -0500

    Run scripts working fine on both, Linux and Windows

--- start.bat
+++ start.bat
@@ -1,6 +1,4 @@
-SET IMAGE_NAME=max-batch-service
-SET CONTAINER_NAME=max-batch-service
 docker network create devspods.com
 docker run --rm -d --name mysql -e MYSQL_ROOT_PASSWORD=E3ZpsFw78QHQGat6 -p 3306:3306 --net devspods.com mysql:8.0.26
-docker build -t %IMAGE_NAME% .
-docker run --rm -it -v %1:/app -w /app --name %CONTAINER_NAME% --net devspods.com %IMAGE_NAME%
+docker build -t batch-base-img .
+docker run --rm -it -v %1:/app -w /app --name max-batch-service --net devspods.com batch-base-img
--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-IMAGE_NAME=maxbatchservice
-CONTAINER_NAME=maxbatchservice
-sudo docker rm -f $CONTAINER_NAME && \
-sudo docker rm -f mysql && \
-sudo docker network create devspods.com && \
+sudo docker rm -f max-batch-service
+sudo docker rm -f mysql 
+sudo docker network create devspods.com 
 sudo docker run --rm -d --name mysql -e MYSQL_ROOT_PASSWORD=E3ZpsFw78QHQGat6 -p 3306:3306 --net devspods.com mysql:8.0.26 && \
-sudo docker build -t $IMAGE_NAME . && \
-sudo docker run --rm -it -v $1:/app -w /app --name $CONTAINER_NAME --net devspods.com $IMAGE_NAME
+sudo docker build -t batch-base-img . && \
+sudo docker run --rm -it -v $1:/app -w /app --name max-batch-service --net devspods.com batch-base-img
